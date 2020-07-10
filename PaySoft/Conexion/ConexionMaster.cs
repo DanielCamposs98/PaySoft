bd1ad8bdb4e68eb728c651bc2625f855bf01f73a
Daniel Campos <Danielcampossm98@gmail.com>
2020-07-09 18:46:19 -0600

    Initial commit

---
+++ PaySoft/Conexion/ConexionMaster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PaySoft.Conexion
+{
+    class ConexionMaster
+    {
+        public static string conn = Convert.ToString(Desencryptacion.checkServer());
+    }
+}
---
+++ PaySoft/Librerias/Desencryptacion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PaySoft.Conexion
+{
+    public class Desencryptacion
+    {
+        static private AES aes = new AES();
+        static public string CnString;
+        static string dbcnString;
+        static public string appPwdUnique = "RESTAURANTE.PaYSoFT.codigo3312.BASESOFT";
+
+
+        public static object checkServer()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("ConnectionString.xml");
+            XmlElement root = doc.DocumentElement;
+            dbcnString = root.Attributes[0].Value;
+            CnString = (aes.Decrypt(dbcnString, appPwdUnique, int.Parse("256")));
+            return CnString;
+
+        }
+
+        internal class label
+        {
+
+        }
+        public static object UsuariosEncryp()
+        {
+            XmlDocument doc = new XmlDocument();
+            label root = new label();
+
+            dbcnString = root.ToString();
+            CnString = (aes.Decrypt(dbcnString, appPwdUnique, int.Parse("256")));
+            return CnString;
+
+        }
+    }
+}
